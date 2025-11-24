dbb86e2cfb2dc6d8d60c547b638d18c86701fa76
Alex-Shukeloits <aleksandrsukelojt@gmail.com>
2025-11-24 12:20:12 +0300

    Create providers.tf

---
+++ terraform/providers.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "bpg/proxmox"
+      version = "0.46.4"
+    }
+  }
+}
+
+provider "proxmox" {
+  endpoint = "https://100.97.185.188:8006/api2/json"
+  api_token = "terraform@pve!{{token_id}}={{token_value}}" 
+  insecure = true
+  }
+  
+
