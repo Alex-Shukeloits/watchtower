0498f62a52ad5c93bf40c2c9c59b2b81dc1e23f3
Alex-Shukeloits <aleksandrsukelojt@gmail.com>
2026-01-06 11:02:15 +0300

    Update providers.tf

--- vms-on-different-nodes/providers.tf
+++ vms-on-different-nodes/providers.tf
@@ -6,10 +6,16 @@ terraform {
     }
   }
 }
+variable "proxmox_api_token" {
+  description = "Proxmox API token for authentication"
+  type        = string
+  sensitive   = true  # Hides value in logs
+}
 
 provider "proxmox" {
   endpoint = "https://100.97.185.188:8006/api2/json"
-  api_token = "terraform@pve!tkn={{token}}"
+  ###api_token = "terraform@pve!tkn={{token}}"
+  api_token = var.proxmox_api_token
   insecure = true
   }
   
